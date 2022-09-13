0288d9e2599e18cef0835e6db54548af6a7c84d1
Stian Lystad <Stianlystad_1@hotmail.com>
2022-09-13 12:40:00 +0000

    complete challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Sidebar = (props) => {
+    return <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+    </div>
+}
+
+//function Sidebar() {
+//    return <h1>Hello from React!</h1>
+//}
+
+export default Sidebar
